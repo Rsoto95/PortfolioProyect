9099c822a1d5df0d19faa37c2d5e3505dcd4fbc1
Rsoto95 <roberto-soto.com@hotmail.com>
2022-12-23 22:18:43 -0600

    Starting commits

---
+++ src/components/skillset/skillset.tsx
@@ -0,0 +1,19 @@
+import "./skillset.css";
+
+const Skillset = () => {
+  return (
+    <section className="skillSetSection">
+      <h1>Skillset</h1>
+      <div className="skills">
+        <div>Hello</div>
+        <div>Hello</div>
+        <div>Hello</div>
+        <div>Hello</div>
+        <div>Hello</div>
+        <div>Hello</div>
+      </div>
+    </section>
+  );
+};
+
+export default Skillset;
